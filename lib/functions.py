4b6f70060a8c9089668b2cf1942da6c1c5c06333
Blair Crumbly <blaircrumbly@gmail.com>
2025-02-18 09:58:30 -0500

    all tests passing

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return(num1 + num2)
+
+#   If the function is called with an argument that isn't a number, it should return null:
+#   const result = halve("two")
+#   => null
 
 def halve(number):
-    pass
+    if type(number) not in [float, int]: 
+        return (None)
+    return number / 2
+
+
+print(halve(4))
+print(halve("4"))
---
+++ lib/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def halve(number):
+    if type(int) != int: 
+        return (None)
+    return number / 2
+
+
+print(halve(4))
+print(halve("4"))
